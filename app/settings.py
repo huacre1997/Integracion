96ca9dfd955c3c557088666853f227b3a6f12be9
Joel <huacreenciso97@gmail.com>
2021-02-27 20:44:01 -0500

    heroku

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pnfnh$12%rkgz*kf81$1jbzc$f-5)i5ago4f!#7si6m0jr5_(c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = [".herokuapp.com"]
 ALLOWED_HOSTS = ["*",".herokuapp.com"]
@@ -100,16 +100,16 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "bdprueba",
-        "HOST": "localhost",
-        "USER": "root",
-        "PASSWORD": "",
-        "PORT": ""
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': "bdprueba",
+#         "HOST": "localhost",
+#         "USER": "root",
+#         "PASSWORD": "",
+#         "PORT": ""
+#     }
+# }
 
 
 
@@ -130,13 +130,13 @@ DATABASES = {
 #     }
 # }
 
-# import dj_database_url
-# from decouple import config
-# DATABASES={
-#     "default":dj_database_url.config(
-#         default=config("DATABASE_URL")
-#     )
-# }
+import dj_database_url
+from decouple import config
+DATABASES={
+    "default":dj_database_url.config(
+        default=config("DATABASE_URL")
+    )
+}
 
 
 # Password validation
@@ -176,21 +176,21 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'console': {
-#             'class': 'logging.StreamHandler',
-#         },
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['console'],
-#              'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-#         },
-#     },
-# }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 #Location of static files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
